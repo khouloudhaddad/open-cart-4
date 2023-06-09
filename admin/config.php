03c4a7c522c954f9da8854e4d1073709d6a3bb14
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-06-09 21:42:02 +0100

    Some changes

--- admin/config.php
+++ admin/config.php
@@ -13,7 +13,7 @@ define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
 define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
-define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_STORAGE', 'C:/xampp/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
